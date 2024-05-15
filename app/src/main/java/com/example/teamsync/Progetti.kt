7de51f04f5e45601680f2862193405eaec201b4e
Bargi20 <129332985+Bargi20@users.noreply.github.com>
2024-05-15 10:55:08 +0200

    Aggiunta la schermata Progetti

--- app/src/main/java/com/example/teamsync/Progetti.kt
+++ app/src/main/java/com/example/teamsync/Progetti.kt
@@ -1,58 +1,63 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.teamsync
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.teamsync.ui.theme.Grey
+import com.example.teamsync.ui.theme.Main
 import com.example.teamsync.ui.theme.SottotitoliGrey
 
 
+@ExperimentalMaterial3Api
 @Composable
-fun Progetti(){
+fun Progetti() {
 
 
-
-    Box(modifier = Modifier
-        .background(Grey)
-        .fillMaxSize()){
-        Column(modifier = Modifier
+    Box(
+        modifier = Modifier
+            .background(Grey)
             .fillMaxSize()
-            .align(Alignment.Center)
-            .padding(top = 50.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .offset(y = -40.dp)
+                .align(Alignment.Center)
+                .padding(top = 50.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-            Text(text = "I Tuoi Progetti",
+        ) {
+            Text(
+                text = "I Tuoi Progetti",
                 fontSize = 24.sp,
                 color = Color.Black,
-                fontFamily = FontFamily.Monospace)
-            Text(text = "Scegli il progetto che vuoi aprire",
+                fontFamily = FontFamily.Monospace
+            )
+            Text(
+                text = "Scegli il progetto che vuoi aprire",
                 color = SottotitoliGrey,
                 modifier = Modifier.padding(top = 10.dp)
             )
@@ -80,20 +85,41 @@ fun Progetti(){
                 )
             }
 
-            Text(text = "Oops!!! Sembra che non ci siano progetti,\n inizia ora creandone uno", color = SottotitoliGrey, modifier = Modifier.offset(y = -140.dp))
-            Button(onClick = { /*TODO*/ }, shape = CircleShape, modifier = Modifier
-                .size(60.dp)
-                .offset(x = 150.dp, y = 70.dp)) {
-                Row {
-                    Icon(Icons.Default.Add, contentDescription = "Tasto Più",
-                        modifier = Modifier.fillMaxSize(),
-                        color = )
-                }
-                
-            }
+            Text(
+                text = "Oops!!! Sembra che non ci siano progetti,\n inizia ora creandone uno",
+                color = SottotitoliGrey,
+                modifier = Modifier.offset(y = -140.dp)
+            )
+        }
+    }
+
+
+
+    BottomSheetScaffold(sheetContent = {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .height(300.dp)
+        ) {
+            Text(text = "cosa andremo a mettere")
+        }
+    }) {
+        Row(modifier = Modifier.offset(y = 750.dp)) {
+            IconButton(
+                onClick = { }, modifier = Modifier
+                    .weight(10f)
+                    .offset(x = 160.dp, y = 40.dp)
+            ) {
+                Icon(
+                    Icons.Default.AddCircle,
+                    contentDescription = null,
+                    tint = Main,
+                    modifier = Modifier.size(40.dp)
+                )
             }
         }
     }
+}
 
 
 

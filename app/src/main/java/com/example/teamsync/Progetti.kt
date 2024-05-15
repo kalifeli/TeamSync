296e16ebeab3c621d69c977512959ae6e679e823
kalifeli <felizianiale34@gmail.com>
2024-05-15 18:54:10 +0200

    modificato il nome dei colori

--- app/src/main/java/com/example/teamsync/Progetti.kt
+++ app/src/main/java/com/example/teamsync/Progetti.kt
@@ -2,29 +2,21 @@
 
 package com.example.teamsync
 
-import android.transition.CircularPropagation
-import android.widget.ImageButton
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -47,10 +39,9 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.teamsync.ui.theme.Grey
-import com.example.teamsync.ui.theme.Grey_Forte
-import com.example.teamsync.ui.theme.Main
-import com.example.teamsync.ui.theme.SottotitoliGrey
+import com.example.teamsync.ui.theme.Grey20
+import com.example.teamsync.ui.theme.Red70
+import com.example.teamsync.ui.theme.Grey70
 import com.example.teamsync.ui.theme.White
 
 
@@ -61,12 +52,12 @@ fun Progetti() {
 
     Box(
         modifier = Modifier
-            .background(Grey)
+            .background(Grey20)
             .fillMaxSize()
     ) {
         Column(
             modifier = Modifier
-                .offset(y = -40.dp)
+                .offset(y = (-40).dp)
                 .align(Alignment.Center)
                 .padding(top = 50.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -79,14 +70,14 @@ fun Progetti() {
             )
             Text(
                 text = "Scegli il progetto che vuoi aprire",
-                color = SottotitoliGrey,
+                color = Grey70,
                 modifier = Modifier.padding(top = 10.dp)
             )
             Box(
                 modifier = Modifier
                     .size(300.dp)
 
-                    .offset(y = -130.dp)// Riduci il margine inferiore per spostare l'immagine più in alto
+                    .offset(y = (-130).dp)// Riduci il margine inferiore per spostare l'immagine più in alto
             ) {
                 Image(
                     painterResource(id = R.drawable.linea),
@@ -97,10 +88,10 @@ fun Progetti() {
             Box(
                 modifier = Modifier
                     .size(240.dp)
-                    .offset(y = -150.dp) // Riduci il margine inferiore per spostare l'immagine più in alto
+                    .offset(y = (-150).dp) // Riduci il margine inferiore per spostare l'immagine più in alto
             ) {
                 Image(
-                    painterResource(id = R.drawable.pana),
+                    painterResource(id = R.drawable.no_project_illustration),
                     contentDescription = "Immagini di Progetti Vuota",
                     modifier = Modifier.fillMaxSize()
                 )
@@ -108,8 +99,8 @@ fun Progetti() {
 
             Text(
                 text = "Oops!!! Sembra che non ci siano progetti,\n inizia ora creandone uno",
-                color = SottotitoliGrey,
-                modifier = Modifier.offset(y = -140.dp)
+                color = Grey70,
+                modifier = Modifier.offset(y = (-140).dp)
             )
 
             val sheetState = rememberModalBottomSheetState()
@@ -128,7 +119,7 @@ fun Progetti() {
                 Icon(
                     Icons.Default.AddCircle,
                     contentDescription = null,
-                    tint = Main,
+                    tint = Red70,
                     modifier = Modifier.size(40.dp)
                 )
             }
--- app/src/main/java/com/example/teamsync/ui/theme/Theme.kt
+++ app/src/main/java/com/example/teamsync/ui/theme/Theme.kt
@@ -10,21 +10,27 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    // TODO: cambiare i colori per la Dark mode
+    primary = Red70 ,
+    secondary = White,
+    tertiary = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Red70,
+    // TODO: cambiare i colori secondari e terziari
+    secondary = White,
+    tertiary = White,
+    background = Grey20,
+    onPrimary = White,
+    onBackground = Color.Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),

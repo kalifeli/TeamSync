9c43842101d0ac609cf11ff0d2905b47d17f0c59
kalifeli <felizianiale34@gmail.com>
2024-05-15 18:52:50 +0200

    modificato il nome dei colori

--- app/src/main/java/com/example/teamsync/ui/theme/Color.kt
+++ app/src/main/java/com/example/teamsync/ui/theme/Color.kt
@@ -1,19 +1,14 @@
 package com.example.teamsync.ui.theme
 
-import androidx.compose.material3.ButtonColors
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val Main = Color(0xFFC1092A)
-val Grey_Forte = Color(0xFFD9D9D9)
-val Grey = Color(0xFFF6F6F6)
+val Red70 = Color(0xFFC1092A)
+val Grey70 = Color(0xFF79747E)
+val Grey50 = Color(0xFFD9D9D9)
+val Grey20 = Color(0xFFF6F6F6)
 val White = Color(0xFFFFFFFF)
-val SottotitoliGrey = Color(0xFF79747E)
+
+
+
 

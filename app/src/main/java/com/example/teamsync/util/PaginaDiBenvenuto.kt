45b6e234f9d89da7112deea3e40e85c7e1792d93
kalifeli <felizianiale34@gmail.com>
2024-05-17 16:42:25 +0200

    aggiunta sfondi schermate di benvenuto

--- app/src/main/java/com/example/teamsync/util/PaginaDiBenvenuto.kt
+++ app/src/main/java/com/example/teamsync/util/PaginaDiBenvenuto.kt
@@ -22,7 +22,7 @@ sealed class PaginaDiBenvenuto (
         immagine = R.drawable.im_pagina_di_benvenuto2,
         titolo = "Tieni traccia di tutti i tuoi progetti",
         sottotitolo = "",
-        sfondo = R.drawable.sfondo_pagina_di_benvenuto_4
+        sfondo = R.drawable.sfondo_pagina_di_benvenuto2
     )
 
     object TerzaPagina : PaginaDiBenvenuto (
@@ -36,13 +36,13 @@ sealed class PaginaDiBenvenuto (
         immagine = R.drawable.im_pagina_di_benvenuto4,
         titolo = "Resta sempre aggiornato",
         sottotitolo = "Attiva le notifiche di gruppo per non perdere mai un aggiornamento importante o una scadenza imminente nei tuoi progetti di gruppo.",
-        sfondo = R.drawable.sfondo_pagina_di_benvenuto_4
+        sfondo = R.drawable.sfondo_pagina_di_benvenuto2
     )
     object QuintaPagina : PaginaDiBenvenuto (
         immagine = R.drawable.im_pagina_di_benvenuto5,
         titolo = "Aggiungi studenti ai tuoi amici",
         sottotitolo = "Costruisci connessioni con nuovi compoagni di corso.",
-        sfondo = R.drawable.sfondo_pagina_di_benvenuto5
+        sfondo = R.drawable.sfondo_pagina_di_benvenuto1
         // rettangolo_bianco
     )
     object SestaPagina : PaginaDiBenvenuto (

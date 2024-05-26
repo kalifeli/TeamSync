8cefdfe497da71d1a9d082def1ede02075e389a3
jackw1-tech <107569512+jackw1-tech@users.noreply.github.com>
2024-05-26 09:45:33 +0200

    Implementazione modifica profilo

--- app/src/main/java/com/example/teamsync/screen/Impostazioni.kt
+++ app/src/main/java/com/example/teamsync/screen/Impostazioni.kt
@@ -1,41 +1,58 @@
 package com.example.teamsync.screen
 
-import androidx.compose.foundation.layout.*
-
-import androidx.compose.material3.Text
-import androidx.compose.material3.Button
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.teamsync.R.drawable.user_icon
-import com.example.teamsync.R.drawable.background
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import com.example.teamsync.R.drawable.icon
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.teamsync.R
 import com.example.teamsync.navigation.Schermate
 import com.example.teamsync.ui.theme.Grey20
 import com.example.teamsync.ui.theme.Red70
 import com.example.teamsync.ui.theme.White
 
+data class SettingItem(val icon: ImageVector, val label: String)
 
+val settingsList = listOf(
+    SettingItem( Icons.Default.Settings, "Modifica Profilo"),
+    SettingItem(Icons.Default.Settings, "Notifiche"),
+    SettingItem(Icons.Default.Settings, "Lingua"),
+    SettingItem(Icons.Default.Settings, "DashBoard"),
+    SettingItem(Icons.Default.Settings, "Task"),
+    SettingItem(Icons.Default.Settings, "To-Do List"),
+    SettingItem(Icons.Default.Settings, "Help & Support"),
+    SettingItem(Icons.Default.Settings, "Report a problem"),
+    SettingItem(Icons.Default.Settings, "Log out")
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Impostazioni(navController: NavHostController) {
-    val background: Painter = painterResource(id = background)
+    val background: Painter = painterResource(id = R.drawable.bianco)
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -50,9 +67,6 @@ fun Impostazioni(navController: NavHostController) {
 
         )
 
-
-
-
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -64,49 +78,114 @@ fun Impostazioni(navController: NavHostController) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(0.2f),
+                    .fillMaxHeight(0.08f),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Image(
-                    painter = painterResource(id = icon),
-                    contentDescription = "Icona Applicazione",
+
+                FloatingActionButton(
+                    onClick = { navController.navigate(Schermate.Impostazioni.route) },
                     modifier = Modifier
-                        .size(70.dp)
+                        .padding(5.dp)
+                        .size(35.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "add project",
+                        tint = Color.Red,
 
 
-                )
+                        )
+                }
+
+                // Centra il testo all'interno della Row
+                Row(
+                    modifier = Modifier.weight(8f),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Impostazioni",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+                // Row vuota per bilanciare il layout
+                Row(
+                    modifier = Modifier.weight(1f),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {}
+
             }
 
-            Spacer(modifier = Modifier.height(20.dp))
-            Image(
-                painter = painterResource(id = user_icon),
-                contentDescription = "Icona User Login",
-                modifier = Modifier.size(150.dp) // Imposta la dimensione dell'immagine
-            )
-            Spacer(modifier = Modifier.height(40.dp))
-            Text(text = "Impostazioni", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-            Spacer(modifier = Modifier.height(25.dp))
-            Button(
-                onClick = { navController.navigate(Schermate.ModificaProfilo.route) },
-                modifier = Modifier.fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(Color(0xFFC1092A))
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
 
-            { Text(text = "ModificaProfilo") }
+            ) {
 
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.05f)
+                        .background(color = Color.LightGray),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+
+                ) {
+
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "ModificaProfilo",
+                        tint = Color.Black,
+                    )
+
+
+                    // Centra il testo all'interno della Row
+                    Row(
+                        modifier = Modifier.clickable { navController.navigate(Schermate.ModificaProfilo.route) }
+                            .weight(8f),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Impostazioni",
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+
+                            )
+                    }
+
+                }
+
+                }
+            }
         }
     }
 
+
+
+@Composable
+fun SettingItemRow(setting: SettingItem) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable { /* Handle item click */ }
+    ) {
+        Icon(imageVector = setting.icon, contentDescription = null)
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = setting.label)
+    }
 }
-
-
-
 
 @Preview
 @Composable
-fun P() {
-    Impostazioni(navController = (rememberNavController()))
+fun PreviewImpostazioni() {
+    Impostazioni(navController = rememberNavController())
 }
-
-

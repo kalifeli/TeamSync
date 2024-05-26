56503380322c9467adb6ea1f2915116f5779d4bf
jackw1-tech <107569512+jackw1-tech@users.noreply.github.com>
2024-05-26 19:24:04 +0200

    Implementazione impostazioni

--- app/src/main/java/com/example/teamsync/screen/Impostazioni.kt
+++ app/src/main/java/com/example/teamsync/screen/Impostazioni.kt
@@ -2,28 +2,29 @@ package com.example.teamsync.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,23 +32,28 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.teamsync.R
 import com.example.teamsync.navigation.Schermate
-import com.example.teamsync.ui.theme.Grey20
-import com.example.teamsync.ui.theme.Red70
-import com.example.teamsync.ui.theme.White
 
-data class SettingItem(val icon: ImageVector, val label: String)
+data class SettingItem(val icon: Int, val label: String, val rotta: Schermate)
 
-val settingsList = listOf(
-    SettingItem( Icons.Default.Settings, "Modifica Profilo"),
-    SettingItem(Icons.Default.Settings, "Notifiche"),
-    SettingItem(Icons.Default.Settings, "Lingua"),
-    SettingItem(Icons.Default.Settings, "DashBoard"),
-    SettingItem(Icons.Default.Settings, "Task"),
-    SettingItem(Icons.Default.Settings, "To-Do List"),
-    SettingItem(Icons.Default.Settings, "Help & Support"),
-    SettingItem(Icons.Default.Settings, "Report a problem"),
-    SettingItem(Icons.Default.Settings, "Log out")
+val settingsList1 = listOf(
+    SettingItem(R.drawable.person_icon, "Modifica Profilo",Schermate.ModificaProfilo ),
+    SettingItem(R.drawable.notifiche, "Notifiche",Schermate.Progetti),
+    SettingItem(R.drawable.lingua, "Lingua",Schermate.Progetti),
+    SettingItem(R.drawable.tema, "Tema",Schermate.Progetti),
+    SettingItem(R.drawable.task, "Task", Schermate.Progetti),
+    SettingItem(R.drawable.to_do_list, "To-Do List",Schermate.Progetti),
+
 )
+
+val settingsList2 = listOf(
+    SettingItem(R.drawable.help, "Help & Support",Schermate.Progetti),
+    SettingItem(R.drawable.terms_and_condiction, "Terms and Policies",Schermate.Progetti)
+)
+
+val settingsList3 = listOf(
+    SettingItem(R.drawable.logout, "Logout",Schermate.Login)
+)
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -74,114 +80,199 @@ fun Impostazioni(navController: NavHostController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
 
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.08f),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-
-                FloatingActionButton(
-                    onClick = { navController.navigate(Schermate.Impostazioni.route) },
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .size(35.dp)
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = "add project",
-                        tint = Color.Red,
-
-
-                        )
-                }
-
-                // Centra il testo all'interno della Row
-                Row(
-                    modifier = Modifier.weight(8f),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = "Impostazioni",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-
-                // Row vuota per bilanciare il layout
-                Row(
-                    modifier = Modifier.weight(1f),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {}
-
-            }
-
+        )
+        {
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 12.dp),
+                    .fillMaxWidth(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
 
             ) {
-
-
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .fillMaxHeight(0.05f)
-                        .background(color = Color.LightGray),
+                        .fillMaxHeight(0.08f),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween
-
                 ) {
 
-                    Icon(
-                        imageVector = Icons.Default.Person,
-                        contentDescription = "ModificaProfilo",
-                        tint = Color.Black,
-                    )
-
+                    FloatingActionButton(
+                        onClick = { navController.navigate(Schermate.Progetti.route) },
+                        modifier = Modifier
+                            .size(35.dp)
+                            .background(Color.White),
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "close_impostazioni",
+                            )
+                    }
 
                     // Centra il testo all'interno della Row
                     Row(
-                        modifier = Modifier.clickable { navController.navigate(Schermate.ModificaProfilo.route) }
-                            .weight(8f),
+                        modifier = Modifier.weight(8f),
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.Center
                     ) {
                         Text(
                             text = "Impostazioni",
                             fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold,
-
-                            )
+                            fontWeight = FontWeight.Bold
+                        )
                     }
 
-                }
+                    // Row vuota per bilanciare il layout
+                    Row(
+                        modifier = Modifier.weight(1f),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {}
 
                 }
             }
+
+            Row(
+                modifier = Modifier.clickable { navController.navigate(Schermate.ModificaProfilo.route) }
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .padding(top = 20.dp)
+                    .padding(bottom = 5.dp),
+            ) {
+                Text(
+                    text = "Account",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+
+                    )
+            }
+
+
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp,  bottomEnd = 12.dp, bottomStart = 20.dp),)
+            ) {
+                items(settingsList1) { setting ->
+                    SettingItemRow(setting = setting, navController = navController)
+                }
+            }
+
+            Row(
+                modifier = Modifier.clickable { navController.navigate(Schermate.ModificaProfilo.route) }
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .padding(top = 50.dp)
+                    .padding(bottom = 5.dp),
+            ) {
+                Text(
+                    text = "Supporto",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+
+                    )
+            }
+
+
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp,  bottomEnd = 12.dp, bottomStart = 20.dp),)
+            ) {
+                items(settingsList2) { setting ->
+                    SettingItemRow(setting = setting, navController = navController)
+                }
+            }
+            Row(
+                modifier = Modifier.clickable { navController.navigate(Schermate.ModificaProfilo.route) }
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .padding(top = 50.dp)
+                    .padding(bottom = 5.dp),
+            ) {
+                Text(
+                    text = "Azioni",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+
+                    )
+            }
+
+
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp,  bottomEnd = 12.dp, bottomStart = 20.dp),)
+            ) {
+                items(settingsList3) { setting ->
+                    SettingItemRow(setting = setting, navController = navController)
+                }
+            }
+
+
         }
-    }
+
+            }
+
+        }
+
+
 
 
 
 @Composable
-fun SettingItemRow(setting: SettingItem) {
+fun SettingItemRow(setting: SettingItem,navController: NavHostController) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
-            .clickable { /* Handle item click */ }
+            .background(Color(0xFFE5E5E5)),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+
     ) {
-        Icon(imageVector = setting.icon, contentDescription = null)
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(text = setting.label)
+
+        Row(
+            modifier = Modifier.weight(0.45f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {}
+        Image(
+            painter = painterResource(id = setting.icon),
+            contentDescription = "Icona",
+            modifier = Modifier
+                .size(40.dp) // Imposta le dimensioni iniziali dell'immagine
+                .padding(
+                    top = 10.dp,
+                    bottom = 8.dp
+                )
+
+        )
+        Row(
+            modifier = Modifier.weight(1.5f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {}
+
+        // Centra il testo all'interno della Row
+        Row(
+            modifier = Modifier.clickable { navController.navigate(setting.rotta.route) }
+                .weight(12f),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Text(
+                text = setting.label,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(
+                    top = 10.dp,
+                    bottom = 8.dp
+                )
+
+            )
+        }
+
     }
+
 }
 
 @Preview
--- app/src/main/java/com/example/teamsync/ui/theme/Color.kt
+++ app/src/main/java/com/example/teamsync/ui/theme/Color.kt
@@ -8,7 +8,7 @@ val Grey70 = Color(0xFF79747E)
 val Grey50 = Color(0xFFD9D9D9)
 val Grey20 = Color(0xFFF6F6F6)
 val White = Color(0xFFFFFFFF)
-
+val Grey35 = Color(0xFFE5E5E5)
 
 val ColorePrioritaAlta = Color(0xFFC1092A)
 val ColorePrioritaMedia = Color(0xFFFFA500)

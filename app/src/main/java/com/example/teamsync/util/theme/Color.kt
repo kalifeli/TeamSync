87e04e9f8732ed9e28007e9568a2128aac02984e
kalifeli <felizianiale34@gmail.com>
2024-08-16 12:07:57 +0200

    aggiunti commenti per alcuni colori utilizzati

---
+++ app/src/main/java/com/example/teamsync/util/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.teamsync.util.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Red70 = Color(0xFFC1092A) // colore principale
+val Grey70 = Color(0xFF79747E)
+val Grey50 = Color(0xFFD9D9D9)
+val Grey20 = Color(0xFFF6F6F6)
+val White = Color(0xFFFFFFFF) // colore principale
+val Grey35 = Color(0xFFE5E5E5)
+val Green50 = Color(0xFF3BC639)
+val WhiteFacebook = Color(0xFFF3F4F8)
+val metaGrigiometaNero = Color(0xFF222222)
+
+val ColorePrioritaAlta = Color(0xFFC1092A) // Rosso per priorità alta
+val ColorePrioritaMedia = Color(0xFFFFA500) // Arancione per priorità media
+val ColorePrioritaBassa = Color(0xFF00C980) // Giallo per priorità bassa
+val ColoreSenzaPriorita = Color(0xFFFFFFFF) // Bianco per nessuna priorità
